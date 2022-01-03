fac121cab78850f617fb8e11a59c166c9e2214c4
Surya1092 <96676153+Surya1092@users.noreply.github.com>
2022-01-03 13:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var audio = "";
+function preload(){
+    audio = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0 ,600 , 500);
+}
+
+function play(){
+    audio.play();
+}
